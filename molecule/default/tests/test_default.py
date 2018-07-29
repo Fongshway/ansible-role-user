61e375c973a8670e3caf7c70412820cf18464399
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-07-29 09:03:27 -0700

    Add var for user shell

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,10 +7,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_user(host):
-    username = host.user("user").name
     home_dir = host.user("user").home
     ssh_key = host.file(home_dir + "/.ssh/id_rsa")
 
-    assert username == "user"
+    assert host.user("user").name == "user"
     assert home_dir == "/home/user"
+    assert host.user("user").shell == "/bin/bash"
     assert ssh_key.exists
